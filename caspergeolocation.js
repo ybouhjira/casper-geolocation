2991257450de74f646b8561e1aed5fc8a34ab864
ybouhjira <b.youssef91@gmail.com>
2014-05-23 19:25:06 +0100

    Initial commit

---
+++ caspergeolocation.js
@@ -0,0 +1,50 @@
+/** casperjs-geolocation
+  * A capser module that mocks the geolocation API
+  */
+
+// functions to be evaluated in remote
+var Remote = {
+  add_geolocation_api : function(pos) {
+    navigator.geolocation = {
+      // store pos here
+      __casperFakeLocation : pos,
+      
+      // getcurrentposition
+      getCurrentPosition : function(callback, err) {
+        callback({coords : this.__casperFakeLocation});
+      },
+
+      // stores the callback passed to watchPosition
+      __casperWatchCallback : undefined,
+
+      // watchposition
+      watchPosition : function(callback) {
+        this.__casperwatchCallback = callback;
+      }
+    };
+  },
+
+  update_position : function(pos) {
+    navigator.geolocation.__casperFakeLocation = pos;
+    navigator.geolocation.__casperWatchCallback(pos);
+  }
+};
+
+// module
+var Geolocation = function(casper, pos) {
+  this._pos = pos;
+  this._casper = casper;
+  this._casper.evaluate(Remote.add_geolocation_api, pos);
+};
+
+Geolocation.prototype.getPos = function () {
+  return this._pos
+};
+
+Geolocation.prototype.setPos = function(pos) {
+  this._pos = pos;
+  this._casper.evaluate(Remote.update_position, pos);
+};
+
+module.exports = Geolocation;
+
